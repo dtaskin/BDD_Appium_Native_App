a0f940b41538c0c124265e78ce817302a0f55811
dtaskin <taskindeniz26@gmail.com>
2022-07-08 22:04:49 -0400

    first commit

--- src/test/java/mobile/pages/ApiDemoHomePage.java
+++ src/test/java/mobile/pages/ApiDemoHomePage.java
@@ -6,14 +6,28 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class ApiDemoHomePage {
 
     public ApiDemoHomePage(AndroidDriver<AndroidElement> androidDriver) {
         PageFactory.initElements(new AppiumFieldDecorator(androidDriver), this);
     }
+    /*
+    resource-id => id
+    accessibility id => accessibility
+     */
 
-    @AndroidFindBy(accessibility = "Accessibility") //accessibility id
+    @AndroidFindBy(accessibility = "Accessibility")
     public AndroidElement accessibilityOption;
 
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Custom View\")")
+    public AndroidElement customView;
+
+    @AndroidFindBy(accessibility = "Views")
+    public AndroidElement viewsOption;
+
+    @AndroidFindBy(id = "android:id/text1")
+    public List<AndroidElement> allMainMenuOptions;
 
 }
--- src/test/java/mobile/utils/Driver.java
+++ src/test/java/mobile/utils/Driver.java
@@ -2,7 +2,6 @@ package mobile.utils;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
@@ -10,7 +9,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-public class DesiredCapabilitiesUtils {
+public class Driver {
+
+    private Driver(){
+
+    }
 
     private static AndroidDriver<AndroidElement> androidDriver;
 
@@ -22,14 +25,16 @@ public class DesiredCapabilitiesUtils {
      * @throws MalformedURLException
      */
 
-    public static AndroidDriver<AndroidElement> setupAndroidDesiredCapabilities() throws MalformedURLException {
+    public static AndroidDriver<AndroidElement> getDriver() throws MalformedURLException {
         if (androidDriver == null) {
             File appFile = new File(ConfigReader.getProperty("apkAppPath")); //stores the app path
             DesiredCapabilities desiredCapabilities = new DesiredCapabilities(); // setting up the mobile device with below values
+
             desiredCapabilities.setCapability("deviceName", ConfigReader.getProperty("deviceName"));
             desiredCapabilities.setCapability("platformName", ConfigReader.getProperty("platformName"));
             desiredCapabilities.setCapability("app", appFile.getAbsolutePath()); // absolute path not content root
-            URL appiumServerUrl = new URL("http://127.0.0.1:4723/wd/hub"); // server url comes in here
+
+            URL appiumServerUrl = new URL("http://127.0.0.1:4723/wd/hub"); // server url comes in here WRITE 127.0.0.1 TO APPIUM SERVER
             androidDriver = new AndroidDriver<>(appiumServerUrl, desiredCapabilities);
             androidDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }
